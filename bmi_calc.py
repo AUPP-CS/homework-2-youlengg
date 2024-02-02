5623c6643dc97a7d6323af68c06ce355ecc3188e
youlengg <2024001ghov@aupp.edu.kh>
2024-02-02 21:09:40 +0700

    The Answer

--- bmi_calc.py
+++ bmi_calc.py
@@ -9,25 +9,29 @@ Reminders:
 '''
 def bmi_check(weight, height):
     # Add your code here
-    try: # here we use the try becuase we want to trying in convert weight and height to float
-        weight = float(weight) # converting weight to float
-        height = float(height) # converting height to float
-        if weight <= 0 or height <= 0: # cheking the condition if weight or height is equal or below to 0
-            return 'invalid input' # if the condition is True: we'll return the invalid input, since weight and height should be more than 0
-        else: # but if the condition if false
-            if weight > 625 or height > 2.72: # we still check for some unrealistic condition, if wight is more than 625 kg or height is more than 2.72 meter 
-                return 'unrealistic information' # then it seem unrealistic, so we print unrealistic
-            else: # if not then all the input are valid and meet the realistic condition
+    try:                        # here we use the try becuase we want to try in converting weight and height to float, if input can't convert then it will return an error
+        weight = float(weight)                      # converting weight to float
+        height = float(height)                      # converting height to float
+        if weight <= 0 or height <= 0:              # cheking the condition if weight or height is equal or below to 0
+            return 'invalid input'                  # if the condition is True: we'll return the invalid input, since weight and height should be more than 0
+        else:                                       # but if weight or float both are not below or equal to 0, it mean that weight > 0, height > 0 is True.
+            if weight > 625 or height > 2.72:       # we still check for some unrealistic condition, if weight is more than 625 kg or height is more than 2.72 meter 
+                return 'unrealistic information'    # then it seem unrealistic, so we print unrealistic
+            else:                                   # if not then all the input are valid and meet the realistic condition
                 result_bmi = weight/(height*height) # so, we do the calculation
                 if result_bmi < 18.5:
-                    return 'underweight', round(result_bmi, ndigits=2)
+                    return 'underweight', round(result_bmi, ndigits=1)
                 elif result_bmi > 18.5 and result_bmi <= 24.9:
                     return 'normal', round(result_bmi, ndigits=1)
                 elif result_bmi > 24.9 and result_bmi <= 29.9:
-                    return 'overweight', round(result_bmi, ndigits=1)
-                elif result_bmi >29.9 and result_bmi <=40:
-                    return 'obese', round(result_bmi, ndigits=2)
+                    return 'overweight', round(result_bmi, ndigits=1)               # we use round to round the result, since it return a lot of decimal
+                elif result_bmi >29.9 and result_bmi <=40:              
+                    return 'obese', round(result_bmi, ndigits=1)       #the function will return two values, one values is the stauts, and another one is the calculted bmi result
                 elif result_bmi > 40:
-                    return 'extremely obese', round(result_bmi, ndigits=1) # we use round to round the result, since it return a lot of decimal
-    except ValueError: # so, if the input from user is anything that it can't convert to float then the code will cause an error, but we counter the error by the except
-        return 'invalid input' # here, instead of showing error, we return the 'invalid input' instead
+                    return 'extremely obese', round(result_bmi, ndigits=1) 
+    except ValueError:        # so, if the input from user is anything that it can't convert to float then the code will cause an error, but we counter the error by the except
+        return 'invalid input'              # here, instead of showing error, we return the 'invalid input' instead
+    
+
+    ### incase the input is invalid or seem unrealistic, the function will only return one value which is the status of it, to show that if it's invalid input or unrealistic.
+    ### but if all the input are correct, then the function will return 2 values, one value is status of user, and another one is the calculated bmi result of user.
--- main.py
+++ main.py
@@ -1,25 +1,89 @@
 from bmi_calc import bmi_check
 
 # Call the bmi_calc function and add in a user interface for the bmi calculator (ex. welcome message, instructions, etc.)
-print("\n Welcome to our app!")
-print("\n ------------------------------------------------------")
+print("""\n                                                                                                                                                                                                                                                                                 
+     __ __  __ __  __ __  __ __  __ __  __ __  __ __  __ __  __ __  __ __  __ __  __ __  __ __  __ __  __ __ 
+    |__ __||__ __||__ __||__ __||__ __||__ __||__ __||__ __||__ __||__ __||__ __||__ __||__ __||__ __||__ __|
+     _                                                                                                    _  
+    | |                                                                                                  | |  
+    | |                                                                                                  | | 
+    |_|                                                                                                  |_| 
+     _     ____   __  __  ___    ____     _     _      ____  _   _  _         _   _____  ___   ____       _  
+    | |   | __ ) |  \/  ||_ _|  / ___|   / \   | |    / ___|| | | || |       / \ |_   _|/ _ \ |  _ \     | | 
+    | |   |  _ \ | |\/| | | |  | |      / _ \  | |   | |    | | | || |      / _ \  | | | | | || |_) |    | | 
+    |_|   | |_) || |  | | | |  | |___  / ___ \ | |___| |___ | |_| || |___  / ___ \ | | | |_| ||  _ <     |_| 
+     _    |____/ |_|  |_||___|  \____|/_/   \_\|_____|\____| \___/ |_____|/_/   \_\|_|  \___/ |_| \_\                                                                                                _  
+    | |                                                                                                  | | 
+    | |                                                                                                  | | 
+    |_|                                                                                                  |_| 
+     __ __  __ __  __ __  __ __  __ __  __ __  __ __  __ __  __ __  __ __  __ __  __ __  __ __  __ __  __ __ 
+    |__ __||__ __||__ __||__ __||__ __||__ __||__ __||__ __||__ __||__ __||__ __||__ __||__ __||__ __||__ __|
+                                                                                                                                                                                                                                                                                                                                                                                                           
+\n""")
+
+#First, showing user some instruction.
+print("💪💪💪 Welcome to Good Health BMI Calculator 💪💪💪")
+print("\n➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+print("To know your Body Mass Index, Please follow through these below instructions. ⬇️  ⬇️  ⬇️")
+print("1️⃣  .Enter your name.")
+print("2️⃣  .Enter your weight in kg.")
+print("3️⃣  .Enter your height in meter.")
+print("4️⃣  .See your result.")
+print("\n➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+
 # Add your code here
 while True:
-    user_name = input("Your name: ")
-    user_weight = input("Your weight: ")
-    user_height = input("Your height: ")
-    returned = bmi_check(user_weight, user_height) #returned represent 2 return values from the function
-    if type(returned) == tuple:    # here we check the type of returned value , this case if it is tuple mean that it retured more than one value
-        status, result_bmi = returned # then create new variable which is status and result_bmi is to unpakced the tuple to two different value which is represent by status and result_bmi
-        print(f'Your status is: {status} and your bmi is: {result_bmi}') 
-    elif returned == 'invalid input': # but if it's invalid then the function will only return one value since in bmi_calc we only give it to returned one value
-        print('Invalid input') # if the returned value is 'invalid input', it will print invalid input
-    elif returned == 'unrealistic information': # the same, if it's unrealistic information, it also return only 1 value
-        print('Unrealistic information') # and it will print unrealistic information
+    #Asking user for three input, name, weight, and height.
+    user_name = input("\n👉👉👉  Enter your name📋📋📋: ")
+    user_weight = input("👉👉👉  Enter your weight in kg ⏲️ ⏲️ ⏲️ : ")
+    user_height = input("👉👉👉  Enter your height in meter📏📏📏: ")
+    
+    #variable returned represent 2 return values from the function
+    returned = bmi_check(user_weight, user_height)
 
-    check_condition = input('Do you want to start again? y/n: ') # here is the condition for the loop, we ask user if they wish to continue
-    if check_condition != 'n': # if check condition not equal to n, which mean user don't want to stop
-        True # then it will return the True for the loop to continue
+    # here we check the type of returned value , this case if the returned value is tuple mean that it retured multiple value packed together.
+    if type(returned) == tuple:
+        # then we create new variable which is status and result_bmi is to unpakced the tuple to two different values and store in status, and result_bmi    
+        status, result_bmi = returned
+
+        #print the result
+        print("\n➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+        print("|                                                                                |")
+        print(f'       💪💪💪 {user_name}, you are {status} and your bmi is: {result_bmi} 💪💪💪 ')
+        print("|                                                                                |")
+        print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+
+    # but if first condition isn't True, then it mean that the function only return one value since in bmi_calc function sometime it returns only one value base on user's input
+
+    # so here we come to check what is that one valued returned, looking at bmi_calc.py, there are two possible values, which are invalid input, and unrealistic information
+    
+    # if the returned value is 'invalid input', the code will print invalid input
+    elif returned == 'invalid input':
+        print("\n➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+        print("|                                                                                |")
+        print('                         ❌❌❌ Invalid input ❌❌❌')
+        print("|                                                                                |")
+        print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+
+    # the same, if that one returned value is unrealistic information, the code will print unrealistic information
+    elif returned == 'unrealistic information':
+        print("\n➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+        print("|                                                                                |")
+        print('                    🚫🚫🚫  Unrealistic information  🚫🚫🚫')
+        print("|                                                                                |")
+        print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+
+    # here is the condition for the loop, we ask user if they wish to continue or not.
+    check_condition = input('\n🔄🔄🔄 Do you want to start again? 🔄🔄🔄 y/n: ').lower()
+
+    # if check condition not equal to n, it means that user don't want to stop, so the code return True to continue the loop.
+    if check_condition != 'n':
+        print("\n🔁🔁🔁🔁🔁🔁🔁🔁🔁🔁🔁🔁🔁🔁🔁🔁🔁🔁... restarted")
+        print("\n")
+        True
+    
+    # if user wish not to continue, we'll print message and break the loop.
     else:
-        print("Thank you for using our app!")
-        break # else, the loop will break
+        print("\n➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+        print("\nThank you for using our app 🥰 🥰 🥰")
+        break
