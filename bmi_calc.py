d6adf2494177e320aa906d2a954aecbae3556a71
youlengg <2024001ghov@aupp.edu.kh>
2024-02-02 14:37:12 +0700

    First Try

--- bmi_calc.py
+++ bmi_calc.py
@@ -9,3 +9,25 @@ Reminders:
 '''
 def bmi_check(weight, height):
     # Add your code here
+    try: # here we use the try becuase we want to trying in convert weight and height to float
+        weight = float(weight) # converting weight to float
+        height = float(height) # converting height to float
+        if weight <= 0 or height <= 0: # cheking the condition if weight or height is equal or below to 0
+            return 'invalid input' # if the condition is True: we'll return the invalid input, since weight and height should be more than 0
+        else: # but if the condition if false
+            if weight > 625 or height > 2.72: # we still check for some unrealistic condition, if wight is more than 625 kg or height is more than 2.72 meter 
+                return 'unrealistic information' # then it seem unrealistic, so we print unrealistic
+            else: # if not then all the input are valid and meet the realistic condition
+                result_bmi = weight/(height*height) # so, we do the calculation
+                if result_bmi < 18.5:
+                    return 'underweight', round(result_bmi, ndigits=2)
+                elif result_bmi > 18.5 and result_bmi <= 24.9:
+                    return 'normal', round(result_bmi, ndigits=1)
+                elif result_bmi > 24.9 and result_bmi <= 29.9:
+                    return 'overweight', round(result_bmi, ndigits=1)
+                elif result_bmi >29.9 and result_bmi <=40:
+                    return 'obese', round(result_bmi, ndigits=2)
+                elif result_bmi > 40:
+                    return 'extremely obese', round(result_bmi, ndigits=1) # we use round to round the result, since it return a lot of decimal
+    except ValueError: # so, if the input from user is anything that it can't convert to float then the code will cause an error, but we counter the error by the except
+        return 'invalid input' # here, instead of showing error, we return the 'invalid input' instead
--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 from bmi_calc import bmi_check
 
 # Call the bmi_calc function and add in a user interface for the bmi calculator (ex. welcome message, instructions, etc.)
-
+print("\n Welcome to our app!")
+print("\n ------------------------------------------------------")
 # Add your code here
+while True:
+    user_name = input("Your name: ")
+    user_weight = input("Your weight: ")
+    user_height = input("Your height: ")
+    returned = bmi_check(user_weight, user_height) #returned represent 2 return values from the function
+    if type(returned) == tuple:    # here we check the type of returned value , this case if it is tuple mean that it retured more than one value
+        status, result_bmi = returned # then create new variable which is status and result_bmi is to unpakced the tuple to two different value which is represent by status and result_bmi
+        print(f'Your status is: {status} and your bmi is: {result_bmi}') 
+    elif returned == 'invalid input': # but if it's invalid then the function will only return one value since in bmi_calc we only give it to returned one value
+        print('Invalid input') # if the returned value is 'invalid input', it will print invalid input
+    elif returned == 'unrealistic information': # the same, if it's unrealistic information, it also return only 1 value
+        print('Unrealistic information') # and it will print unrealistic information
+
+    check_condition = input('Do you want to start again? y/n: ') # here is the condition for the loop, we ask user if they wish to continue
+    if check_condition != 'n': # if check condition not equal to n, which mean user don't want to stop
+        True # then it will return the True for the loop to continue
+    else:
+        print("Thank you for using our app!")
+        break # else, the loop will break
